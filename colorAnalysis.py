283bb25606885e1fc20c035c6b7f62c27b88b981
Chan Pham <tqp2001@columbia.edu>
2019-04-01 14:25:53 -0400

    colored image writing option, fuller color breakdown

--- colorAnalysis.py
+++ colorAnalysis.py
@@ -6,26 +6,35 @@ class ColorAnalysis:
     def __init__(self):
         self.bucketer = ColorBucketer()
 
-    def colorBreakdown(self, image):
-        histogram = dict.fromkeys(self.bucketer.color_bases.keys(), 0)
+    def colorBreakdown(self, image, outputImage=False):
+        imageOut = []
+        if outputImage:
+            imageOut = image.copy()
+            self.bucketer.reset_bias()
+            print(self.bucketer.BIAS, self.bucketer.color_bases_bgr['white'], self.bucketer.color_bases_bgr['black'])
+
+        histogram = dict.fromkeys(self.bucketer.color_bases_bgr.keys(), 0)
         rows, cols, depth = image.shape
         for r in range(rows):
             for c in range(cols):
-                histogram[self.bucketer.bucket_color(image[r, c])] += 1
+                color = self.bucketer.bucket_color(image[r, c])
+                histogram[color] += 1
+                if outputImage:
+                    imageOut[r, c] = self.bucketer.color_bases_bgr[color]
         total = float(sum(histogram.values()))
-        print(total)
+        # print(total)
         h2 = {}
-        valid = 0
+        valid = []
         for color in histogram:
             print color, histogram[color]
             h2[color] = histogram[color]/total * 100
             if h2[color] > 5:
-                valid += 1
+                valid.append(color)
 
-        return (h2, valid)
+        return (h2, valid, imageOut)
 
         
-    def brightness(self, img):
+    def brightness_bgr(self, img):
         img_bnw = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         brightness = img_bnw.mean()/255.0 * 100
         return brightness
@@ -34,13 +43,26 @@ class ColorAnalysis:
 def main():
     a = ColorAnalysis()
 
-    img = cv.imread(sys.argv[1], cv.IMREAD_COLOR) #default BGR
-    colSummary = a.colorBreakdown(img) 
-    print(colSummary)
+    img_bgr = cv.imread(sys.argv[1], cv.IMREAD_COLOR) #default BGR
+    
+    colBreakdown, colPresent, imgOut = a.colorBreakdown(img_bgr, True) 
+    print(colBreakdown)
+    cv.imshow('imageIn', img_bgr)
+    cv.imshow('imageOut', imgOut)
 
     # cv.imshow("bnw", img_bnw)
-    # cv.waitKey(0)
-    print(a.brightness(img))
+    cv.waitKey(0)
+    try:
+        save = raw_input("To save, input file name. Else, press enter.\n")
+        cv.imwrite(save, imgOut)
+    except (EOFError, ValueError):
+        pass
+        
+    cv.destroyAllWindows()
+    # print(a.brightness_bgr(img_bgr))
+
+    # img_hsv = cv.cvtColor(img_bgr, cv.COLOR_BGR2HSV)
+
 
 
 main()
