c68db2a752f8eaa016daeb1eed16b83de36cf993
Chan Pham <tqp2001@columbia.edu>
2019-03-31 23:17:27 -0400

    updated to percentage breakdown, added number of colors and brightness

--- colorAnalysis.py
+++ colorAnalysis.py
@@ -1,22 +1,46 @@
 from colorIdentifier import ColorBucketer
 import cv2 as cv
+import sys
 
 class ColorAnalysis: 
     def __init__(self):
         self.bucketer = ColorBucketer()
 
-    def evalHistogram(self, image):
+    def colorBreakdown(self, image):
         histogram = dict.fromkeys(self.bucketer.color_bases.keys(), 0)
         rows, cols, depth = image.shape
         for r in range(rows):
             for c in range(cols):
                 histogram[self.bucketer.bucket_color(image[r, c])] += 1
-        return histogram
+        total = float(sum(histogram.values()))
+        print(total)
+        h2 = {}
+        valid = 0
+        for color in histogram:
+            print color, histogram[color]
+            h2[color] = histogram[color]/total * 100
+            if h2[color] > 5:
+                valid += 1
 
-a = ColorAnalysis()
+        return (h2, valid)
 
-img = cv.imread('rainbow.png', cv.IMREAD_COLOR) #default BGR
-img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB) #colAn needs RGB
+        
+    def brightness(self, img):
+        img_bnw = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        brightness = img_bnw.mean()/255.0 * 100
+        return brightness
 
-hist = a.evalHistogram(img)
-print(hist)
+
+def main():
+    a = ColorAnalysis()
+
+    img = cv.imread(sys.argv[1], cv.IMREAD_COLOR) #default BGR
+    colSummary = a.colorBreakdown(img) 
+    print(colSummary)
+
+    # cv.imshow("bnw", img_bnw)
+    # cv.waitKey(0)
+    print(a.brightness(img))
+
+
+main()
