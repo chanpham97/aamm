6850e866f6976167862e0ef408b30da47b1267c8
vasudharengarajan <vasudha.rengarajan@gmail.com>
2019-04-14 17:13:58 -0400

    Wrote get_track_features for music matcher

--- analysis/music-match.py
+++ analysis/music-match.py
@@ -1,19 +1,34 @@
-import requests
-import base64
+from __future__ import print_function    # (at top of module)
+from spotipy.oauth2 import SpotifyClientCredentials
 import config
-import path
+import json
+import spotipy
+import time
+import sys
+    
+def get_track_features(client_id, config_id, tid):
+    client_credentials_manager = SpotifyClientCredentials(client_id, config_id)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-def get_token():
-    header = {'Authorization': 'Basic ' + base64.b64encode(config.SPOTIFY_CLIENT_ID + ':' + config.SPOTIFY_CLIENT_SECRET).encode('ascii')}
-    r = requests.post(path.SPOTIFY_AUTH, data={'grant_type': 'client_credentials'}, headers=header)
-    return r.json()["access_token"]
+    start = time.time()
+    features = sp.audio_features(tid)
+    analysis = sp.audio_analysis(tid)
+    delta = time.time() - start
+    
+    json_dump = json.dumps(features, indent=4)
+    print(json_dump)
+    json_features = json.loads(json_dump)
+    print ("analysis retrieved in %.2f seconds" % (delta,))
+    
+    entry = y[0]
+    return entry["energy"], entry["loudness"], entry["tempo"], entry["key"]
+    
+
+get_track_features(config.SPOTIFY_CLIENT_ID, config.SPOTIFY_CLIENT_SECRET, 'spotify:track:52lJakcAPTde2UnuvEqFaK')
+get_track_features(config.SPOTIFY_CLIENT_ID, config.SPOTIFY_CLIENT_SECRET, 'spotify:track:0PfQd8JoZTLC7QmuSALrnH')
 
 
-def get_id(name, typ):
-    headers = {'Authorization': 'Bearer ' + get_token()}
-    query = {'q': name, 'type': typ}
-    r = requests.get(path.SPOTIFY_SEARCH, headers=headers, params=query)
-    return r.json()[typ + 's']
-
-
-print(get_id('instrumental study', 'playlist'))
+############# NOTES ####################
+# one key feature is: energy, loudness, tempo. maybe key would allow us to classify (happy v sad for the colors)
+#tid = 'spotify:track:52lJakcAPTde2UnuvEqFaK' # angry, fast, percussive, orchestral
+#tid = 'spotify:track:0PfQd8JoZTLC7QmuSALrnH' # one instrument, peaceful, calm
