f0511e377ea0fb684c74da40a445a742c0ff1d08
Chan Pham <tqp2001@columbia.edu>
2019-04-14 23:34:30 -0400

    route updates

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import random
+import sys
+sys.path.insert(0, "../analysis/")
+from musicArtMatcher import MusicArtMatcher 
+import config
+
 app = Flask(__name__)
- 
-@app.route("/")
-def hello():
-    paintings_dict = {
+
+paintings_dict = {
+        "0.jpg": ["Painting with red spot", "Wassily Kandinsky", "1914"],
         "1.jpg": ["Lausanne Abstract", "Francis Picabia", "1918"],
         "2.jpg": ["Glass Painting with the Sun (Small Pleasures)", "Wassily Kandinsky", "1910"],
         "3.jpg": ["Bild no. 84", "Jacoba van Heemskerck", "1918"],
@@ -18,12 +22,26 @@ def hello():
         "11.jpg": ["With full force", "Lyubov Popova", ""],
         "12.jpg": ["Eroun", "Wolfgang Paalen", "1944"],
         "13.jpg": ["1949-A-No.1", "Clyfford Still", "1949"],
-        "14.jpg": ["The Air", "Joan Miro", "1937"],
-        "15.jpg": ["Painting with red spot", "Wassily Kandinsky", "1914"]
-        }
+        "14.jpg": ["The Air", "Joan Miro", "1937"]
+}
+
+tracks_list = [
+    "52lJakcAPTde2UnuvEqFaK", "0PfQd8JoZTLC7QmuSALrnH", "1LqFdwLKqa8Ep6q9LEUCih", "2Yb67ozAhETHhy5i5eIDI1",
+    "6cPbVV2I3AjhSHxB5J4Ozd", "0nF5aQoLs2YtbWwClXvumL", "2mbdpLcDqFsA5efI0LJn5i",
+    "6C5iTxvpG5Geb66InRxoSP"
+]
+
+@app.route("/")
+def hello():    
     painting, painting_info = random.choice(list(paintings_dict.items()))
-    track = "https://open.spotify.com/embed/track/2XKFnwB6djxrJCjR3PdeRb?si=SfXkZ-koQ-e8uW8FiCo8tA"
+    matcher = MusicArtMatcher(config.SPOTIFY_CLIENT_ID, config.SPOTIFY_CLIENT_SECRET, 100)
+    track_id = matcher.match('../app/static/images/', painting, tracks_list)
+    print(track_id)
+    # painting = str(painting_index % len(paintings_dict.keys())) + ".jpg"
+    # painting_info = paintings_dict[painting]
+    track = "https://open.spotify.com/embed/track/" + track_id
     return render_template('index.html', painting_path="/static/images/" + painting, title=painting_info[0], artist=painting_info[1], track_url=track)
  
+
 if __name__ == "__main__":
     app.run()
