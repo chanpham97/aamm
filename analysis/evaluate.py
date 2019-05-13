12469d70b26451587d7e479d95ebfc7748610554
vasudharengarajan <vasudha.rengarajan@gmail.com>
2019-05-13 13:08:06 -0400

    Updated evaluate.py print statements

--- analysis/evaluate.py
+++ analysis/evaluate.py
@@ -54,21 +54,26 @@ plt.show()
 
 for key in sorted(dic.keys()):
     cluster_counts = Counter(dic[key])
-    print("Cluster ", key, ": ", cluster_counts)
+    #print("Cluster ", key+1, ": ", cluster_counts)
+    print("Cluster {}:  (1: {}), (0: {}), (-1: {})".format(key+1,cluster_counts[1], cluster_counts[0], cluster_counts[-1]) )
     plt.bar(list(cluster_counts.keys()), cluster_counts.values())
     plt.show()
 
 print("*************************************")
 print()
 
-print("Total", ": ", total_counts)
-total_counts_percent_list = [(i, round( (total_counts[i] / len(list(df_new["grade"]))), 2)) for i in total_counts]
-print("In percentage", ": ", total_counts_percent_list)
+#print("Total", ": ", total_counts)
+print("Total:  (1: {}), (0: {}), (-1: {})".format(total_counts[1], total_counts[0], total_counts[-1]) ) 
+total_counts_percent_list = sorted([(i, round( (total_counts[i] / len(list(df_new["grade"]))), 2)) for i in total_counts], reverse=True)
+#print("In percentage", ": ", total_counts_percent_list)
+print("In percentage:  (1: {}), (0: {}), (-1: {})".format(key+1, total_counts_percent_list[0][1], total_counts_percent_list[1][1], total_counts_percent_list[2][1]) ) 
+
 print()
 
 for key in sorted(dic.keys()):
     cluster_counts = Counter(dic[key])
-    print("Cluster ", key, ": ", cluster_counts)
+    print("Cluster {}:  (1: {}), (0: {}), (-1: {})".format(key+1,cluster_counts[1], cluster_counts[0], cluster_counts[-1]) )
+    #print("Cluster ", key+1, "1:{}. 0:{}. -1:{}", "".format(cluster_counts[1], cluster_counts[0], cluster_counts[-1]))
     #cluster_counts_percent_list = [(i, cluster_counts[i] / len(dic[key])) for i in cluster_counts]
     #print("In percentage:")
     #print(cluster_counts_percent_list)
@@ -78,7 +83,11 @@ for key in sorted(dic.keys()):
     cluster_counts = Counter(dic[key])
     #print("Cluster ", key, ": ", cluster_counts)
     cluster_counts_percent_list = [(i, round( (cluster_counts[i] / len(dic[key])), 2)) for i in cluster_counts]
-    print("Cluster ", key, ": ", cluster_counts_percent_list)
+    l = sorted(cluster_counts_percent_list, reverse=True)
+    #print("Cluster ", key+1, ": ", )
+    print("Cluster {}:  (1: {}), (0: {}), (-1: {})".format(key+1, l[0][1], l[1][1], l[2][1]) )
+    
+    
     #print()
 
 print()    
